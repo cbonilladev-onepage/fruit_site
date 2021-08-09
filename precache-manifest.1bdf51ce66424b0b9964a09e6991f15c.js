1a2eb862351b9a7eeab2a6278cba910fb84e69c8
cabonilla <christian.bonilla01@gmail.com>
2021-08-09 15:02:02 -0700

    Updates

---
+++ precache-manifest.1bdf51ce66424b0b9964a09e6991f15c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4080f11737bceebe6ad89c924d3fa7ce",
+    "url": "/fruit_site/index.html"
+  },
+  {
+    "revision": "c2aa50fa5c7b1dcad549",
+    "url": "/fruit_site/static/css/main.d36c95a0.chunk.css"
+  },
+  {
+    "revision": "cead2f2de54ec3b42f8e",
+    "url": "/fruit_site/static/js/2.4d6e5200.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/fruit_site/static/js/2.4d6e5200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2aa50fa5c7b1dcad549",
+    "url": "/fruit_site/static/js/main.4f1d3f8b.chunk.js"
+  },
+  {
+    "revision": "ad855ba06fb699bc9a92",
+    "url": "/fruit_site/static/js/runtime-main.9c00465e.js"
+  },
+  {
+    "revision": "f454b1ec787b47c446fbfd0b5cc32952",
+    "url": "/fruit_site/static/media/compressed-image.f454b1ec.jpg"
+  },
+  {
+    "revision": "4946a5903771c1575480561a0eb45eee",
+    "url": "/fruit_site/static/media/fruit.4946a590.webp"
+  },
+  {
+    "revision": "aa8bb45836e4f173572edcba020b553e",
+    "url": "/fruit_site/static/media/fruit.aa8bb458.jpg"
+  }
+]);
